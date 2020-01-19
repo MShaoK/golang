e2a14c029533db042903ed11a14c61b2a0d97a77
Shao Kang Mai <Colinmaisk@gmail.com>
2020-01-18 22:16:14 -0800

    finished basics of golang tutorial

--- hello.go
+++ hello.go
@@ -1,12 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-	"math"
+import "fmt"
 
+const (
+	Big = 1 << 100
+	Small = Big >> 99
 )
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
 
 func doNothing(x, y, z int) (int, int, int){
 	return x , y , z
@@ -22,10 +26,7 @@ func nakedReturn(sum int) (x, y int){
 func main() {
 	fmt.Println(nakedReturn(10))
 	fmt.Println(doNothing(50, 100, 1))
-	fmt.Println("Welcome to the playground")
-	fmt.Println("My favorite number is", rand.Intn(10))
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
-	fmt.Println(math.Pi)
-	fmt.Println("The time is", time.Now())
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
 }
 
