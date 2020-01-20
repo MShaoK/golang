37d14f5e1619d31c0df02eac2ebd75370e2b67f9
Shao Kang Mai <Colinmaisk@gmail.com>
2020-01-19 21:08:39 -0800

    testing out switch statement

--- hello.go
+++ hello.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
-func Sqrt(x float64) float64 {
-	z := 1.0
-	z -= (z*z - x) / (2*z)
-	return z
-}
-
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
 }
 
