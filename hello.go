df1a03913e6fff0b8e2490c8053bf2177e58aa56
Shao Kang Mai <Colinmaisk@gmail.com>
2020-01-20 23:35:57 -0800

    tested structs syntax

--- hello.go
+++ hello.go
@@ -9,16 +9,5 @@ type Vertex struct {
 
 func main() {
 	fmt.Println(Vertex{1,2})
-
-	i, j := 42, 2701
-
-	p := &i         // point to i
-	fmt.Println(*p) // read i through the pointer
-	*p = 21         // set i through the pointer
-	fmt.Println(i)  // see the new value of i
-
-	p = &j         // point to j
-	*p = *p / 37   // divide j through the pointer
-	fmt.Println(j) // see the new value of j
 }
 
