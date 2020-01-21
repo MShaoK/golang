e438d31b53972b125cbccd3c32246e8807680c0d
Shao Kang Mai <Colinmaisk@gmail.com>
2020-01-20 23:55:47 -0800

    testing slice defaults

--- hello.go
+++ hello.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	s := []int{2, 3, 5, 7, 11, 13}
 
-	var s []int = primes[1:4]
+	s = s[1:4]
+	fmt.Println(s)
+
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
 	fmt.Println(s)
 }
 
