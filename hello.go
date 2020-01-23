306444a48b7798c235a4de7619b4b0819586360f
Shao Kang Mai <Colinmaisk@gmail.com>
2020-01-22 23:40:30 -0800

    playing with range

--- hello.go
+++ hello.go
@@ -1,27 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	// Create a tic-tac-toe board.
-	board := [][]string{
-		[]string{"_", "_", "_"},
-		[]string{"_", "_", "_"},
-		[]string{"_", "_", "_"},
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) // == 2**i
 	}
-
-	// The players take turns.
-
-	board[0][0] = "X"
-	//board[2][2] = "O"
-	//board[1][2] = "X"
-	board[1][0] = "O"
-	board[0][2] = "X"
-
-	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
 	}
 }
+
