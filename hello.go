20f50de10450a0e633e497830e684b335a9c8ae0
Shao Kang Mai <Colinmaisk@gmail.com>
2020-02-02 23:45:17 -0800

    trying out point receivers

--- hello.go
+++ hello.go
@@ -9,12 +9,17 @@ type Vertex struct {
 	X, Y float64
 }
 
-func Abs(v Vertex) float64 {
+func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(Abs(v))
-	fmt.Println("no need for go build?")
+	v.Scale(10)
+	fmt.Println(v.Abs())
 }
