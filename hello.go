7d096668a909c4c8138d82eaa29987aa751f0235
Shao Kang Mai <Colinmaisk@gmail.com>
2020-02-09 23:10:05 -0800

    empty interface

--- hello.go
+++ hello.go
@@ -1,42 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-type Abser interface {
-	Abs() float64
-}
+import "fmt"
 
 func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
+	var i interface{}
+	describe(i)
 
-	a = f  // a MyFloat implements Abser
-	a = &v // a *Vertex implements Abser
+	i = 42
+	describe(i)
 
-	// In the following line, v is a Vertex (not *Vertex)
-	// and does NOT implement Abser.
-
-	fmt.Println(a.Abs())
+	i = "hello"
+	describe(i)
 }
 
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
-
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }
 
